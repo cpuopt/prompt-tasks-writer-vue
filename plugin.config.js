dfc9a6bceec89982ca70286fa075803e1c239cfe
cpufan <554950835@qq.com>
2024-06-12 23:30:56 +0800

    修改禁用动画效果的方式

--- plugin.config.js
+++ plugin.config.js
@@ -3,7 +3,7 @@ const pluginConfig = {
     userscript: {
         icon: 'https://www.google.com/s2/favicons?sz=64&domain=novelai.net',
         namespace: 'https://www.cpufan.top/index.php/2024/03/18/novelai-prompts-task-writer/',
-        version: '0.0.2.240603',
+        version: '0.0.2.240612',
         match: ['https://novelai.net/image'],
         author: 'cpuopt',
         description: {
